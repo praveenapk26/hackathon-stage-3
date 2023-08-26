72fe0ddcddfc5074739cd1663b3fe9a439cff522
praveenapk26 <rahanepraveen@gmail.com>
2023-08-26 14:00:19 +0000

    Added skeleton code for all api

---
+++ progress-tracking-api/src/test/java/com/isp/progresstrackingapi/ProgressTrackingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isp.progresstrackingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressTrackingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
